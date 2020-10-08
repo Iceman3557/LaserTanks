cbb2eee3d10a81faa09c3c6d7414f081f310fc62
Iceman3557 <45889395+Iceman3557@users.noreply.github.com>
2020-10-08 20:03:34 +0800

    Finished Master-Slave communication
    Master to slave and viceversa is done :)

--- Arena/Master/Master.ino
+++ Arena/Master/Master.ino
@@ -6,18 +6,18 @@
  */
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#define T1 12345678   
-#define T2 12345678
+#define T1 "41001558216874129"   
+#define T2 "41051558216874129"
 #define T3 12345678
 #define POWERUP_NUM 3
 
  
 bool check =false;
 bool in = false;
-int powerupType[POWERUP_NUM]; // [p0,p1...pn]
 const int ledPin =53;
 const int interruptPin = 3;
-uint8_t collisionBuffer[POWERUP_NUM][2]; //[tank ID, powerup type] 
+uint8_t collisionBuffer[POWERUP_NUM][2]; //[tank ID, powerup type]
+int cycle[POWERUP_NUM]; 
 //
 LiquidCrystal_I2C lcd(0x27,16,2);  
 void setup() 
@@ -36,21 +36,25 @@ void setup()
 
 void loop() 
 {
+    int ii;
    if(!digitalRead(interruptPin)) // testing
    {
     ii =1;
-    cycle[ii]++;
-    cycle[ii] = cycle[ii]%6; 
+    cycle[ii-1]++;
+    cycle[ii-1] = cycle[ii-1]%6; 
     Serial.println("Testing Transmission:");
    int a[] = {1,15,cycle[ii]};
+   collisionBuffer[ii-1][1]=cycle[ii-1]; // for powerup ii, store powertype
    transmissionEvent(a); // For Slave 1, set duration to be 5 seconds for speed powerup
+   Serial.print(collisionBuffer[ii-1][0]);
+   Serial.println(collisionBuffer[ii-1][1]);
    }
    
   for(int jj=1;jj<3;jj++)
   //if(0)
   {
       Wire.requestFrom(jj,7);
-      receiving();
+      receiving(jj);
       // insert code for transmit data to IDE
   }
   //printloop(); not sure what this does so ill leave it in
@@ -71,44 +75,68 @@ void printHex(unsigned int numberToPrint)
  * Name: receiving
  * Purpose: Sets the data to zero and read in the 8 bits from the respective wire
  */
-void receiving()
+void receiving(int jj)
 { 
+  String str = "";
+  int tankID[7];
+  delay(5);
 
+  while (Wire.available()>6)
+  {
+    // loop through all but the last
+    for (int ii=0; ii < 7; ii++)
+    {
+      tankID[ii] = Wire.read(); // receive byte as a character
+      str += tankID[ii]; // store ID as a string
+    }
+
+    if(str != "0255255255255255255") // if ID not invalid 
+    {
+      Serial.println(str);
+      collisionBuffer[jj-1][0] = tankIdentify(str);
+    }
+  }
+    
  }
 
- void tankIdentify(String str)
+ int tankIdentify(String str)
  {
-    int tankId = 0;
-    if str== T1;
+    int tank = 0;
+    if (str == T1)
     {
-       tankId = 1;
+      tank  = 1;
     }
-    if str== T2;
+    if (str == T2)
     {
-       tankId = 2;
+      tank  = 2;
     }
-    if str== T3;
+    if (str == T3)
     {
-       tankId = 3;
+      tank  = 3;
     }
+    Serial.println(tank);
+    return tank;
  }
-  
+ 
+ 
   void receiveEvent()
   {
-    int powerup_info[2]; // Name,Duration
+    /*int powerup_info[3]; // Name,Duration,type
     while (0 < Wire.available())
      {
-          ii = Wire.read();// Slave Number
+          powerup_info[0] = Wire.read();// Slave Number
           powerup_info[1]=Wire.read();// Duration for power up
+          powerup_info[3] = Wire.read();// Powerup Type
           transmissionEvent(powerup_info); //
      }
+     */
   }
-  void transmissionEvent(int trans[0])
+  void transmissionEvent(int trans[])
   {
       Wire.beginTransmission(trans[0]); // powerup number
       Wire.write(trans[1]); // the duration
       Wire.write(trans[2]); // powerupType
-      Wire.endTransmission(1);
+      Wire.endTransmission(trans[0]);
       Serial.println("Sent val");
   }
 
